16cea1216833aa68326e2e4200c4fffd020fe851
husneiremkaya <ireemkaayaaa@gmail.com>
2021-06-07 15:34:30 +0300

    Restoran Web Sitesi
    ASP.NET Teknolojileri ve MS-SQL ile bir Restorant Sitesi
    ve Veri Tabanı Uygulaması

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebProje.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Creat()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Creat(FormCollection form)
+        {
+            return View();
+        }
+    }
+}
